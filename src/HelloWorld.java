15df672a54376bbd878a3bafab468e7c47bf46b7
firedot1 <zcheng1@montgomerycollege.edu>
2020-04-21 09:13:17 -0400

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main (String Args[]) {
+        System.out.println("Hello, World!");
+        System.out.println("Today is July 19, 2015. I am alive!");
+        System.out.println("My Name is Alton");
+    }
+}
